32fc03eab1693260e7d8ca2241fb265715f942e7
Harry <shaikmohammadibrahim311@gmail.com>
2023-09-15 22:33:09 +0530

    get hello2

--- src/main/java/com/progrank/Main.java
+++ src/main/java/com/progrank/Main.java
@@ -1,7 +1,9 @@
 package com.progrank;
 
 import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Primary;
 
 
@@ -10,7 +12,7 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("Hello world2");
 		System.out.println("Iam stuffed");
-		ApplicationContext context=  SpringApplication.run(Main.class,args);
+		ConfigurableApplicationContext context=  SpringApplication.run(Main.class,args);
 		
 	}
 
--- src/main/java/com/progrank/controller/TestController.java
+++ src/main/java/com/progrank/controller/TestController.java
@@ -1,11 +1,16 @@
 package com.progrank.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+@GetMapping("/hello")
 public String helloworld() {
 	return "Helloworld";
+}
+@GetMapping("/hello2")
+public String helloworld2() {
+	return "Helloworld2";
 }
 }
